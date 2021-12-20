63ca0aff14ac85495c5d3fd717dacadb18af6b51
Vinícius <matosvb@gmail.com>
2021-12-20 19:58:09 -0300

    Added documentation to server.py

--- blabgddatalake/server.py
+++ blabgddatalake/server.py
@@ -1,14 +1,15 @@
+"""A module that runs a simple HTTP server providing file metadata/contents."""
+
 from flask import abort, Flask, jsonify, request, Response, send_file
 from pathlib import Path
 from structlog import getLogger
 from sys import maxsize
 from waitress import serve as waitress_serve
 
-
 from .local import LocalStorageDatabase, LocalFile, FileToDelete
 
 
-logger = getLogger(__name__)
+_logger = getLogger(__name__)
 
 
 app = Flask(__name__)
@@ -17,6 +18,17 @@ app = Flask(__name__)
 @app.route("/tree", methods=['GET'])
 @app.route("/tree/<id>", methods=['GET'])
 def tree(id: str | None = None) -> Response | None:
+    """Return a file tree containing only metadata.
+
+    Query args:
+        depth (int): maximum depth.
+
+    Args:
+        id: if provided, use a specific file as root of a sub-tree.
+
+    Returns:
+        the response in JSON
+    """
     config = app.config['options']
     db = LocalStorageDatabase(config['Database'])
     depth = request.args.get('depth', maxsize, type=int)
@@ -30,9 +42,18 @@ def tree(id: str | None = None) -> Response | None:
 
 @app.route("/download/<id>/<head_revision_id>", methods=['GET'])
 def file(id: str, head_revision_id: str) -> Response | None:
+    """Return the contents of a file.
+
+    Args:
+        id: file id
+        head_revision_id: id of the current file version
+
+    Returns:
+        the file contents
+    """
     config = app.config['options']
     db = LocalStorageDatabase(config['Database'])
-    log = logger.bind(id=id)
+    log = _logger.bind(id=id)
     with db.new_session() as session:
         f: LocalFile | FileToDelete | None
         f = db.get_file_by_id(session, id)
@@ -54,7 +75,18 @@ def file(id: str, head_revision_id: str) -> Response | None:
             abort(503)
 
 
-def serve(config: dict, port: int) -> int:
+def serve(config: dict, port: int | None) -> int:
+    """Start server.
+
+    Args:
+        config: configuration parameters (see
+            :download:`the documentation <../README_CONFIG.md>`).
+        port: the port to listen on (if provided, overrides the value
+            in ``config['LakeServer']['Port']``)
+
+    Returns:
+        0 if no problems occurred, 1 otherwise
+    """
     server_cfg = config['LakeServer']
     if port is None:
         port = int(p) if (p := server_cfg['Port']) is not None else None
